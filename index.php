3a9ba8cfb4001a77a8c7d3f1f61dc1e79a575a83
ema97 <lotofacundo6@gmail.com>
2020-09-08 12:47:12 -0300

    falta parte back-end

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="formulario">
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+<script src="jquery-3.5.1.min.js"></script>
+<script src="popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>     
+</body>
+</html>
